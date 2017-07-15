9f6d198a0f37bca4f7a40461f5d658d4c14dc8ec
dpritchLibre <dpritch@live.unc.edu>
2017-07-15 12:00:51 -0400

    Create new class CoefGen

--- src/dsp_sampler.cpp
+++ src/dsp_sampler.cpp
@@ -4,57 +4,28 @@
 // [[Rcpp::export]]
 void dsp_sampler(Rcpp::NumericMatrix U) {
 
-    // int n = U.nrow;
-    // int p = U.ncol;
-    int p = 5;
+    WGen W();
+    XiGen xi();
+    CoefGen coefs();
+    PhiGen phi();
 
-    GammaGen** gamma = new GammaGen*[p];
-    GammaGen** gamma_end = gamma + p;
+    for (int s = 0; s < nSamp; s++) {
 
-    for (int h = 0; h < p; ++h) {
-    	*(gamma + h) = new GammaGen(U, h);
+	// update the latent day-specific pregnancy variables W
+	W.sample();
+
+	// update the woman-specific fecundability multipliers xi
+	xi.sample();
+
+	// update the regression coefficients gamma and psi
+	coefs.sample();
+
+	// update exp_u_prod_beta
+
+	// update phi, the variance parameter for xi
+	phi.sample();
     }
 
 
-
-
-    // int s;
-
-    // bool is_burn;
-
-    // vector<GammaGen>::const_iterator var_end = var_gen.end();
-
-
-    // for (int s = 0; s < nSamp; s++) {
-
-	// samp_W();
-
-	// // sample regression coefficients gamma
-	// for (vector<VarGen>::const_iterator curr = var_gen.begin(); curr != var_end; ) {
-	//     gam_val = curr->sample_gam();
-	//     if (++curr != end) {
-	// 	curr->set_ar_param(gam_val);
-	//     }
-	// }
-
-	// // sample woman-specific fecundability multiplier xi
-	// xi = samp_xi();
-
-	// // sample phi, the variance parameter for xi
-	// phi = samp_phi();
-
-
-	// samp_X(X);
-
-	// for (int i = 0; i < var.size(); i++) {
-	//     var.samp_miss(U);
-	// }
-    // }
-
-
-    for (int h = 0; h < p; ++h) {
-    	delete *(gamma + h);
-    }
-    delete gamma;
 
 }
