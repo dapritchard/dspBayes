4d3ca3d0e17b46785612632fa0438191b72d9aa8
dpritchLibre <dpritch@live.unc.edu>
2017-08-06 13:23:35 -0400

    Update dspDat documentation

---
+++ R/dspBayes-package.R
@@ -0,0 +1,12 @@
+#' dspBayes
+#'
+#' This package ....  TODO
+#'
+#' @author David A. Pritchard \email{dpritch@live.unc.edu}
+#'
+#' @name dspBayes
+#' @docType package
+#' @import Rcpp
+#' @importFrom magrittr %>%
+#' @useDynLib dspBayes
+NULL
