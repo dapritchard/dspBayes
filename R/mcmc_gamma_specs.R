e10d15f99c5867ed279632776f8195f062ee2ec5
David Pritchard <dpritch@live.unc.edu>
2019-05-19 20:01:40 -0400

    update gamma_specs for naive model

--- R/mcmc_gamma_specs.R
+++ R/mcmc_gamma_specs.R
@@ -1,13 +1,21 @@
 get_gamma_specs <- function(dsp_data, fw_ar_model) {
 
+    true_vals <- c(0.11, 0.22, 0.44, 0.22, 0.11)
+    a_lookup <- true_vals^2
+    b_lookup <- true_vals
+
     # some temporary glue code.  create gamma specs
     gamma_hyper_list <- vector("list", ncol(dsp_data$U))
     for (i in seq_len(ncol(dsp_data$U))) {
+        stopifnot(i <= 5L)
         gamma_hyper_list[[i]] <- c(type     = 1,
                                    h        = i - 1,
-                                   hyp_a    = 1,
-                                   hyp_b    = 1,
-                                   hyp_p    = 0.5,
+                                   hyp_a    = a_lookup[i],
+                                   hyp_b    = b_lookup[i],
+                                   hyp_p    = 0.00,
+                                   # hyp_a    = 1,
+                                   # hyp_b    = 1,
+                                   # hyp_p    = 0.5,
                                    bnd_l    = 0,
                                    bnd_u    = Inf,
                                    mh_p     = 0.1,
